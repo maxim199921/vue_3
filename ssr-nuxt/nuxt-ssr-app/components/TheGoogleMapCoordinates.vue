82039234a848207dc97c7a325000e5e7ee3f692a
Maxim.Chupilin <maxim.chupilin@hiqo-solutions.com>
2023-09-28 11:24:58 +0400

    add google maps coordinates flag on click

--- ssr-nuxt/nuxt-ssr-app/components/TheGoogleMapCoordinates.vue
+++ ssr-nuxt/nuxt-ssr-app/components/TheGoogleMapCoordinates.vue
@@ -6,19 +6,48 @@
       style="width: 100%; height: 300px"
       @click='setCoordinates'
   >
+    <GMapCluster :zoomOnClick="true">
+      <GMapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="true"
+          @click="center = m.position"
+      />
+    </GMapCluster>
   </GMapMap>
 </template>
 
 
 <script setup lang="ts">
-import { defineEmits, ref } from 'vue'
+import {defineEmits, ref, reactive} from 'vue'
 
 const emit = defineEmits(['coordinates'])
-const center = ref({lat: 41.63196116119842, lng: 41.623328379664116});
+const center = ref(
+  {
+    lat: 41.63196116119842,
+    lng: 41.623328379664116
+  }
+);
+let markers = reactive([
+  {
+    position: {
+      lat: 41.63196116119842,
+      lng: 41.623328379664116,
+    },
+  },
+]);
 
 const setCoordinates = (event: any) => {
   const lat = event.latLng.lat();
   const lng = event.latLng.lng();
-  emit('coordinates', { lat, lng });
+  markers[0] =  {
+    position: {
+      lat,
+      lng,
+    },
+  },
+  emit('coordinates', {lat, lng});
 }
 </script>
